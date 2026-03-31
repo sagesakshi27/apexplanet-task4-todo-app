76231c19fa3fb501d8dc3176d8dfcbdd36a8a483
Chavan Sakshi Pandurang <sakshichavan4151@gmail.com>
2026-03-31 12:51:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+const input = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const list = document.getElementById("taskList");
 
+function addTask() {
+    const task = input.value.trim();
+    if (task === "") return;
+
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = task;
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "X";
+    delBtn.classList.add("delete-btn");
+
+    delBtn.addEventListener("click", () => {
+        li.remove();
+    });
+
+    span.addEventListener("click", () => {
+        li.classList.toggle("completed");
+    });
+
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    list.appendChild(li);
+
+    input.value = "";
+}
+
+addBtn.addEventListener("click", addTask);
+
+input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") addTask();
+});
